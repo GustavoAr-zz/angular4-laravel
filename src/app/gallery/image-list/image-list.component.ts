a1adab8dc2339580fd9e2728e72ed116a86ad60c
Alexis Sánchez <aasanchez@gmail.com>
2017-09-19 16:36:50 +0200

    Add Image Services

--- src/app/gallery/image-list/image-list.component.ts
+++ src/app/gallery/image-list/image-list.component.ts
@@ -1,27 +1,22 @@
 import {Component, OnInit, Input} from '@angular/core';
 import {Image} from '../../models/image';
+import {ImageService} from '../../services/image/image.service';
+
 
 @Component({
   selector: 'ng-image-list',
   templateUrl: './image-list.component.html',
   styles: []
 })
-export class ImageListComponent implements OnInit {
-  images: Image[] = [
-    new Image('1', 'Primera', 'Descripcion primera imagen', 'http://via.placeholder.com/300x200', 'http://via.placeholder.com/800x500'),
-    new Image('2', 'Segunda', 'Descripcion segunda imagen', 'http://via.placeholder.com/300x200', 'http://via.placeholder.com/800x500'),
-    new Image('3', 'Tercera', 'Descripcion tercera imagen', 'http://via.placeholder.com/300x200', 'http://via.placeholder.com/800x500'),
-    new Image('4', 'Cuarta', 'Descripcion cuarta imagen', 'http://via.placeholder.com/300x200', 'http://via.placeholder.com/800x500'),
-    new Image('5', 'Quinta', 'Descripcion quinta imagen', 'http://via.placeholder.com/300x200', 'http://via.placeholder.com/800x500'),
-    new Image('6', 'Sexta', 'Descripcion sexta imagen', 'http://via.placeholder.com/300x200', 'http://via.placeholder.com/800x500'),
-    new Image('7', 'Setptima', 'Descripcion septima imagen', 'http://via.placeholder.com/300x200', 'http://via.placeholder.com/800x500'),
-    new Image('8', 'Octava', 'Descripcion octava imagen', 'http://via.placeholder.com/300x200', 'http://via.placeholder.com/800x500')
-  ];
 
-  constructor() {
+export class ImageListComponent implements OnInit {
+  images: Image[] = [];
+
+  constructor(private imageService: ImageService) {
   }
 
   ngOnInit() {
+    this.images = this.imageService.getImages();
   }
 
 }
---
+++ src/app/services/image/image.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Image } from '../../models/image';
+
+@Injectable()
+export class ImageService {
+  images: Image[] = [
+    new Image('1', 'Primera', 'Descripcion primera imagen', 'http://via.placeholder.com/300x200', 'http://via.placeholder.com/800x500'),
+    new Image('2', 'Segunda', 'Descripcion segunda imagen', 'http://via.placeholder.com/300x200', 'http://via.placeholder.com/800x500'),
+    new Image('3', 'Tercera', 'Descripcion tercera imagen', 'http://via.placeholder.com/300x200', 'http://via.placeholder.com/800x500'),
+    new Image('4', 'Cuarta', 'Descripcion cuarta imagen', 'http://via.placeholder.com/300x200', 'http://via.placeholder.com/800x500'),
+    new Image('5', 'Quinta', 'Descripcion quinta imagen', 'http://via.placeholder.com/300x200', 'http://via.placeholder.com/800x500'),
+    new Image('6', 'Sexta', 'Descripcion sexta imagen', 'http://via.placeholder.com/300x200', 'http://via.placeholder.com/800x500'),
+    new Image('7', 'Setptima', 'Descripcion septima imagen', 'http://via.placeholder.com/300x200', 'http://via.placeholder.com/800x500'),
+    new Image('8', 'Octava', 'Descripcion octava imagen', 'http://via.placeholder.com/300x200', 'http://via.placeholder.com/800x500')
+  ];
+  constructor() { }
+  getImages() {
+    return this.images;
+  }
+}
