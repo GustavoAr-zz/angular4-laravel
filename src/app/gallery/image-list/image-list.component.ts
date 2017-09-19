33a6bdbc28db2ec283e2fa894f0b9f84ea4230de
Alexis Sánchez <aasanchez@gmail.com>
2017-09-19 16:04:36 +0200

    Handle multiple images

--- src/app/gallery/image-list/image-list.component.ts
+++ src/app/gallery/image-list/image-list.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Input} from '@angular/core';
-import { Image } from '../../models/image';
+import {Component, OnInit, Input} from '@angular/core';
+import {Image} from '../../models/image';
 
 @Component({
   selector: 'ng-image-list',
@@ -7,8 +7,19 @@ import { Image } from '../../models/image';
   styles: []
 })
 export class ImageListComponent implements OnInit {
-  image = new Image('1', 'Primera Imagen', 'Descripcion primera imagen', 'https://videotutoriales.com/maspa/maspa1.jpg', 'https://videotutoriales.com/maspa/maspa1-1.jpg')
-  constructor() { }
+  images: Image[] = [
+    new Image('1', 'Primera', 'Descripcion primera imagen', 'http://via.placeholder.com/300x200', 'http://via.placeholder.com/800x500'),
+    new Image('2', 'Segunda', 'Descripcion segunda imagen', 'http://via.placeholder.com/300x200', 'http://via.placeholder.com/800x500'),
+    new Image('3', 'Tercera', 'Descripcion tercera imagen', 'http://via.placeholder.com/300x200', 'http://via.placeholder.com/800x500'),
+    new Image('4', 'Cuarta', 'Descripcion cuarta imagen', 'http://via.placeholder.com/300x200', 'http://via.placeholder.com/800x500'),
+    new Image('5', 'Quinta', 'Descripcion quinta imagen', 'http://via.placeholder.com/300x200', 'http://via.placeholder.com/800x500'),
+    new Image('6', 'Sexta', 'Descripcion sexta imagen', 'http://via.placeholder.com/300x200', 'http://via.placeholder.com/800x500'),
+    new Image('7', 'Setptima', 'Descripcion septima imagen', 'http://via.placeholder.com/300x200', 'http://via.placeholder.com/800x500'),
+    new Image('8', 'Octava', 'Descripcion octava imagen', 'http://via.placeholder.com/300x200', 'http://via.placeholder.com/800x500')
+  ];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
