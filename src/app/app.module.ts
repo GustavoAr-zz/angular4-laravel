d546c1589e70c50051721cedc9b48fb7dbdc4741
Alexis Sánchez <aasanchez@gmail.com>
2017-09-19 14:37:43 +0200

    Installacion de Angular QuickStart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,11 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { NuevoComponent } from './nuevo/nuevo.component';
+import { AppComponent }  from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NuevoComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
