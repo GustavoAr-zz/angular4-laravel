bb7a0312f716b6e15f21868715a0a6357dd2ea92
Alexis Sánchez <aasanchez@gmail.com>
2017-09-26 14:13:48 +0200

    componente admin

--- nggallery/src/app/app.routes.ts
+++ nggallery/src/app/app.routes.ts
@@ -1,13 +1,15 @@
-import {Routes, RouterModule} from '@angular/router';
-import {GalleryComponent} from './gallery/gallery.component';
-import {ContactComponent} from './contact/contact.component';
 import {AboutComponent} from './about/about.component';
+import {AdminComponent} from './admin/admin.component';
+import {ContactComponent} from './contact/contact.component';
+import {GalleryComponent} from './gallery/gallery.component';
 import {ModuleWithProviders} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 const appRoutes: Routes = [
   {path: '', redirectTo: '/gallery', pathMatch: 'full'},
   {path: 'gallery', component: GalleryComponent},
   {path: 'contact', component: ContactComponent},
-  {path: 'about', component: AboutComponent}
+  {path: 'about', component: AboutComponent},
+  {path: 'admin', component: AdminComponent}
 ];
 export const routes: ModuleWithProviders = RouterModule.forRoot(appRoutes);
